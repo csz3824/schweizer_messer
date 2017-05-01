344f35dde7dc486d43244a6abd45ba098783b92b
chensuzhou <chensuzhou@renrenche.com>
2017-05-01 19:00:43 +0800

    modify username

--- test.sql
+++ test.sql
@@ -1,8 +1,8 @@
 
 DROP TABLE IF EXISTS `health`;
 CREATE TABLE `health` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增主键|陈苏洲|2017-04-04',
-  `health` varchar(255) DEFAULT NULL COMMENT '健康监测的值|陈苏洲|2017-04-04',
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增主键|csz3824|2017-04-04',
+  `health` varchar(255) DEFAULT NULL COMMENT '健康监测的值|csz3824|2017-04-04',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
